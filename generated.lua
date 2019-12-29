a69f618d2bb348978ba52f3186ab3f73ea238976
Thomas Rudin <thomas@rudin.io>
2019-12-29 20:58:01 +0100

    .name

--- generated.lua
+++ generated.lua
@@ -12,5 +12,5 @@ function mapcleaner.is_generated(chunk_pos)
 	local min_node = minetest.get_node(min_pos)
 	local max_node = minetest.get_node(max_pos)
 
-	return min_node.name ~= "ignore" or max_node ~= "ignore"
+	return min_node.name ~= "ignore" or max_node.name ~= "ignore"
 end

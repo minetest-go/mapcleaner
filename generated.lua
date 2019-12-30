c96fcee18ccc2b4e1c4fba1ecc3d16658c947295
Thomas Rudin <thomas@rudin.io>
2019-12-30 14:19:48 +0100

    cleanup after geenrate

--- generated.lua
+++ generated.lua
@@ -12,5 +12,14 @@ function mapcleaner.is_generated(chunk_pos)
 	local min_node = minetest.get_node(min_pos)
 	local max_node = minetest.get_node(max_pos)
 
-	return min_node.name ~= "ignore" or max_node.name ~= "ignore"
+	local is_generated = min_node.name ~= "ignore" or max_node.name ~= "ignore"
+
+	if not is_generated then
+		-- clean up afterwards
+		-- looks like the above calls create "ignore" only mapblocks on the database
+		minetest.delete_area(min_pos, min_pos)
+		minetest.delete_area(max_pos, max_pos)
+	end
+
+	return is_generated
 end

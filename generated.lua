4e0f6bb33f1a4ca18b73853060e0a5a4fbfb2b59
BuckarooBanzay <BuckarooBanzay@github.com>
2020-01-07 09:32:47 +0100

    better generated check

--- generated.lua
+++ generated.lua
@@ -5,20 +5,31 @@ function mapcleaner.is_generated(chunk_pos)
 	local min_pos = mapcleaner.get_blocks_from_mapblock(min_mapblock_pos)
 	local _, max_pos = mapcleaner.get_blocks_from_mapblock(max_mapblock_pos)
 
+	local check_pos = { x=min_pos.x, y=min_pos.y, z=min_pos.z }
+
+	if chunk_pos.x < 0 then
+		check_pos.x = max_pos.x
+	end
+
+	if chunk_pos.y < 0 then
+		check_pos.y = max_pos.y
+	end
+
+	if chunk_pos.z < 0 then
+		check_pos.z = max_pos.z
+	end
+
 	-- load area
-	minetest.get_voxel_manip(min_pos, min_pos)
-	minetest.get_voxel_manip(max_pos, max_pos)
+	minetest.get_voxel_manip(check_pos, check_pos)
 
-	local min_node = minetest.get_node(min_pos)
-	local max_node = minetest.get_node(max_pos)
+	local node = minetest.get_node(check_pos)
 
-	local is_generated = min_node.name ~= "ignore" or max_node.name ~= "ignore"
+	local is_generated = node.name ~= "ignore"
 
 	if not is_generated then
 		-- clean up afterwards
 		-- looks like the above calls create "ignore" only mapblocks on the database
-		minetest.delete_area(min_pos, min_pos)
-		minetest.delete_area(max_pos, max_pos)
+		minetest.delete_area(check_pos, check_pos)
 	end
 
 	return is_generated

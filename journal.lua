a0c460de1193f63974ad6fec1dcdbf50c5a0f87d
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-06-26 08:10:38 +0200

    add journal

---
+++ journal.lua
@@ -0,0 +1,16 @@
+
+local journal_path = "mapgen_journal"
+
+minetest.mkdir(journal_path)
+
+-- write generated chunk coordinates to a journal for later cleanup
+minetest.register_on_generated(function(minp, maxp)
+	local date = os.date("*t")
+	local filename = journal_path .. "/" .. date.year .. "-" .. date.month .. "-" .. date.day
+
+	local file = io.open(filename, "a+")
+	if file then
+		file:write(minetest.pos_to_string(minp) .. "_" .. minetest.pos_to_string(maxp) .. "\n")
+		io.close(file)
+	end
+end)

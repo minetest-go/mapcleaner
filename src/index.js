77d36d8b7fd3c731a783065de003ee3ff5ae85b2
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-07-07 09:06:54 +0200

    simple worker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 const app = require("./app");
-const pool = require("./pool");
+const executor = require("./executor");
 
 const port = process.env.PORT || 8080;
 
@@ -8,15 +8,60 @@ app.listen(+port, () => {
   console.log(`Listening on http://127.0.0.1:${port}`);
 });
 
-pool.connect()
-.then(client => {
-  return client.query(`select count(*) from blocks`)
-  .then(sql_res => {
-    console.log(sql_res);
-    client.release();
-  })
-  .catch(e => {
-    client.release();
-    console.error(e.stack);
-  });
+executor(`
+  select
+    count(*) as count,
+    min(posx) as minx,
+    min(posy) as miny,
+    min(posz) as minz,
+    max(posx) as maxx,
+    max(posy) as maxy,
+    max(posz) as maxz
+  from blocks`, [], { single_row: true })
+.then(res => {
+  console.log(res);
+
+  let { minx, miny, minz, maxx, maxy, maxz } = res;
+  const pos = { x: minx, y: miny, z: minz };
+
+  function shift(pos){
+    pos.x++;
+
+    if (pos.x > maxx){
+      pos.x = minx;
+      pos.z++;
+    }
+
+    if (pos.z > maxz){
+      pos.z = minz;
+      pos.y++;
+    }
+
+    if (pos.y > maxy){
+      // done
+      return true;
+    }
+
+    return false;
+  }
+
+  function worker(pos){
+    executor(`
+      select data
+      from blocks
+      where posx = $1 and posy = $2 and posz = $3
+    `, [pos.x,pos.y,pos.z], { single_row: true })
+    .then(block => {
+      console.log(pos, block);
+      const done = shift(pos);
+
+      if (!done){
+        setTimeout(function(){
+          worker(pos);
+        }, 50);
+      }
+    });
+  }
+
+  worker(pos);
 });

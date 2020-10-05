29365c866cc964f5a29f0c33027011e4a5e012e2
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-10-05 11:14:11 +0200

    warn if mapblock not parseable

--- src/mapblockparser.js
+++ src/mapblockparser.js
@@ -1,99 +1,102 @@
 const zlib = require("zlib");
 
 module.exports.parse = data => new Promise(function(resolve, reject) {
-	const buffer = Buffer.from(data);
+	try {
+		const buffer = Buffer.from(data);
 
-	let offset = 0;
+		let offset = 0;
 
-	// version stuff
-	const version = buffer.readUInt8(offset);
+		// version stuff
+		const version = buffer.readUInt8(offset);
 
-	if (version < 25 || version > 28) {
-		return reject("mapblock version not supported: " + version);
-	}
+		if (version < 25 || version > 28) {
+			return reject("mapblock version not supported: " + version);
+		}
 
-	if (version >= 27) {
-		offset = 4;
-	} else {
-		//u16 lighting_complete not present
-		offset = 2;
-	}
+		if (version >= 27) {
+			offset = 4;
+		} else {
+			//u16 lighting_complete not present
+			offset = 2;
+		}
 
-	// content
+		// content
 
-	const content_width = buffer.readUInt8(offset);
-	const params_width = buffer.readUInt8(offset+1);
+		const content_width = buffer.readUInt8(offset);
+		const params_width = buffer.readUInt8(offset+1);
 
-	if (content_width != 2 || params_width != 2){
-		return reject("content/param width mismatch!");
-	}
+		if (content_width != 2 || params_width != 2){
+			return reject("content/param width mismatch!");
+		}
 
-	//mapdata (blocks)
+		//mapdata (blocks)
 
-	if (version >= 27) {
-		offset = 6;
-	} else {
-		offset = 4;
-	}
+		if (version >= 27) {
+			offset = 6;
+		} else {
+			offset = 4;
+		}
 
-	const mapdata_buffer = buffer.subarray(offset);
+		const mapdata_buffer = buffer.subarray(offset);
 
-	let inflate = zlib.createInflate();
-	inflate.on("data", function(/*mapdata*/){
-		offset += inflate.bytesWritten;
-		const metadata_buffer = buffer.subarray(offset);
-		inflate = zlib.createInflate();
-
-		inflate.on("data", function(/*metadata*/){
+		let inflate = zlib.createInflate();
+		inflate.on("data", function(/*mapdata*/){
 			offset += inflate.bytesWritten;
+			const metadata_buffer = buffer.subarray(offset);
+			inflate = zlib.createInflate();
 
-			//static objects version
-			const static_objects_version = buffer.readUInt8(offset);
-			offset++;
+			inflate.on("data", function(/*metadata*/){
+				offset += inflate.bytesWritten;
 
-			const static_objects_count = buffer.readUInt16BE(offset);
-			offset += 2;
+				//static objects version
+				const static_objects_version = buffer.readUInt8(offset);
+				offset++;
 
-			for (let i=0; i < static_objects_count; i++) {
-				offset += 13;
-				const dataSize = buffer.readUInt16BE(offset);
-				offset += dataSize + 2;
-			}
-
-			//timestamp
-			offset += 4;
-
-			//mapping version
-			offset++;
-
-			const numMappings = buffer.readUInt16BE(offset);
-			const node_id_mapping = {};
-
-			offset += 2;
-			for (let i=0; i < numMappings; i++) {
-				const nodeId = buffer.readUInt16BE(offset);
+				const static_objects_count = buffer.readUInt16BE(offset);
 				offset += 2;
 
-				const nameLen = buffer.readUInt16BE(offset);
+				for (let i=0; i < static_objects_count; i++) {
+					offset += 13;
+					const dataSize = buffer.readUInt16BE(offset);
+					offset += dataSize + 2;
+				}
+
+				//timestamp
+				offset += 4;
+
+				//mapping version
+				offset++;
+
+				const numMappings = buffer.readUInt16BE(offset);
+				const node_id_mapping = {};
+
 				offset += 2;
+				for (let i=0; i < numMappings; i++) {
+					const nodeId = buffer.readUInt16BE(offset);
+					offset += 2;
 
-				const blockName = buffer.subarray(offset, offset+nameLen).toString();
-				offset += nameLen;
+					const nameLen = buffer.readUInt16BE(offset);
+					offset += 2;
 
-				node_id_mapping[blockName] = nodeId;
-			}
+					const blockName = buffer.subarray(offset, offset+nameLen).toString();
+					offset += nameLen;
 
-			resolve({
-				version: version,
-				static_objects_count: static_objects_count,
-				static_objects_version: static_objects_version,
-				node_id_mapping: node_id_mapping
+					node_id_mapping[blockName] = nodeId;
+				}
+
+				resolve({
+					version: version,
+					static_objects_count: static_objects_count,
+					static_objects_version: static_objects_version,
+					node_id_mapping: node_id_mapping
+				});
 			});
+
+			inflate.write(metadata_buffer);
 		});
 
-		inflate.write(metadata_buffer);
-	});
-
-	inflate.write(mapdata_buffer);
-
+		inflate.write(mapdata_buffer);
+	} catch (e) {
+		console.error("possible mapblock corruption detected!", e);
+	}
 });

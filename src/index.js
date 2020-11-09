55eaa5970ff62edf90802b6ac14607c507f91d99
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-11-09 09:35:08 +0100

    recheck areas every 5 minutes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const worker = require("./worker");
 const areas = require("./areas");
 
+let area_parse_handler = null;
+
 if (process.env.AREAS_FILE) {
   areas.parse(process.env.AREAS_FILE);
+  // recheck areas every 5 minutes
+  area_parse_handler = setInterval(() => areas.parse(process.env.AREAS_FILE), 5*60*1000);
 }
 
 worker()
 .then(result => {
   console.log(`processed ${result.chunkcount} chunks, removed ${result.removecount} chunks`);
+  if (area_parse_handler !== null){
+    clearInterval(area_parse_handler);
+  }
 });

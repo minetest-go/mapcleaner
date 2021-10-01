c5a59ceb5c2b4bffea2533ba92fc8e8f716bc793
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-10-01 21:07:20 +0200

    working go dep

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module mapcleaner
 
 go 1.17
+
+require github.com/minetest-go/mapparser v0.0.1
--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/minetest-go/mapparser"
+)
 
 func main() {
+	mapparser.Parse([]byte{})
 	fmt.Println("Starting")
 }
